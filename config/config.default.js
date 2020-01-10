be9341950d483749426b3515c062fa804c6145e1
Yueming Fang <zergdo@gmail.com>
2020-01-10 09:31:41 +0800

    feat: support clients

--- config/config.default.js
+++ config/config.default.js
@@ -6,16 +6,16 @@
  * @property {String} SOME_KEY - some description
  */
 exports.sendmail = {
-  client: {
-    from: '"no-reply" <no-reply@localhost>',
-    smtp: {
-      host: 'smtp.local.host', // smtp host
-      port: 465, // smtp port
-      secure: true, // true for 465, false for other ports
-      auth: {
-        user: 'no-reply@local.host', // mail
-        pass: 'password', // password
-      },
-    },
-  },
+  // client: {
+  //   from: '"no-reply" <no-reply@localhost>',
+  //   smtp: {
+  //     host: 'smtp.local.host', // smtp host
+  //     port: 465, // smtp port
+  //     secure: true, // true for 465, false for other ports
+  //     auth: {
+  //       user: 'no-reply@local.host', // mail
+  //       pass: 'password', // password
+  //     },
+  //   },
+  // },
 };
